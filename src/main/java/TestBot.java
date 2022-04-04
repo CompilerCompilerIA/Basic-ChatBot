af49075f77a5f28eb870f046cbd518103b87df3c
Mikaela Cárdenas <60782219+Mik43la@users.noreply.github.com>
2022-04-03 22:55:38 -0400

    Commit Main Class Mikaela Cardenas

--- src/main/java/TestBot.java
+++ src/main/java/TestBot.java
@@ -1,2 +1,20 @@
+package consoleBot;
+
+import java.util.Scanner;
+
 public class TestBot {
+
+    public static void main(String[] args) throws Exception {
+        new ConnectDB();
+        ConnectDB.conn();
+        Scanner sc = new Scanner(System.in);
+        String userInput = "";
+        MyBot chatbot = new MyBot();
+        while(!userInput.equalsIgnoreCase("Stop it")) {
+            System.out.print("User: ");
+            userInput = sc.nextLine();
+            chatbot.answer(userInput);
+        }
+        sc.close();
+    }
 }
